30c3d169eb7d5efb5137c9be18463126ad15b692
Sören Weber <mail@soeren-weber.de>
2023-10-12 00:25:11 +0200

    make chartjs work

--- content/fr/graphs/index.md
+++ content/fr/graphs/index.md
@@ -1,3 +1,4 @@
-## TEST
-
++++
+title = "Graph"
++++
 {{< chartjs id="acquisitions" width="80%" jsFile="sample.js" />}}
--- content/fr/graphs/sample.js
+++ content/fr/graphs/sample.js
@@ -1,5 +1,3 @@
-import Chart from 'chart.js/auto'
-
 (async function() {
   const data = [
     { year: 2010, count: 10 },
