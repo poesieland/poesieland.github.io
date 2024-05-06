6116fd668da00ec60d3d0366c673dd0af5b8e01f
Barbara Post <post.barbara@gmail.com>
2024-05-06 21:35:50 +0200

    Poem by day radar: take into account EN poems

--- content/fr/charts/poem-day-pie.js
+++ content/fr/charts/poem-day-pie.js
@@ -1,8 +1,8 @@
 import { addPieChart } from './add-chart.js'
 (async function () {
   const data = [
-    { label: 'Jours sans écrire', value: 27, color: 'rgba(72, 149, 239, 1)' },
-    { label: 'Jours de création', value: 338, color: 'rgba(76, 201, 240, 1)' },
+    { label: 'Jours sans écrire', value: 25, color: 'rgba(72, 149, 239, 1)' },
+    { label: 'Jours de création', value: 340, color: 'rgba(76, 201, 240, 1)' },
   ];
   addPieChart('poemDayPie', [data], { plugins: { title: { display: true, text: 'Avec ou sans création ?' } } });
 })();
