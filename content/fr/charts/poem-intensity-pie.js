0ec5a58a03fea59f765bf3b401260ebb7872b07b
Barbara Post <post.barbara@gmail.com>
2024-05-06 21:44:41 +0200

    Poems intensity pie chart: take into account EN poems

--- content/fr/charts/poem-intensity-pie.js
+++ content/fr/charts/poem-intensity-pie.js
@@ -1,8 +1,8 @@
 import { addPieChart } from './add-chart.js'
 (async function () {
   const data = [
-    { label: '1 poème', value: 637, color: 'rgba(72, 149, 239, 0.5)' },
-    { label: '2 poèmes', value: 110, color: 'rgba(72, 149, 239, 0.6)' },
+    { label: '1 poème', value: 650, color: 'rgba(72, 149, 239, 0.5)' },
+    { label: '2 poèmes', value: 111, color: 'rgba(72, 149, 239, 0.6)' },
     { label: '3 poèmes', value: 20, color: 'rgba(72, 149, 239, 0.7)' },
     { label: '4 poèmes', value: 4, color: 'rgba(72, 149, 239, 0.8)' },
     { label: '5 poèmes', value: 3, color: 'rgba(72, 149, 239, 0.9)' },
